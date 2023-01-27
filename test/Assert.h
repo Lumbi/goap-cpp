81b7b7d30f1c39c08133dd8c1329c6cefa4f0b33
Gabriel Lumbi <Lumbi@users.noreply.github.com>
2023-01-27 14:19:35 +0100

    Refactor Assert.h

--- test/Assert.h
+++ test/Assert.h
@@ -1,13 +1,38 @@
 #include <iostream>
+#include <string>
+#include <functional>
 
-template<typename A, typename B>
-void ASSERT_EQUAL(const A& a, const B& b, const char* message = nullptr)
+inline void ASSERT(std::function<bool (void)> predicate, const std::string& message)
 {
-    if (a != b) {
-        if (message != nullptr) {
-            std::cout << message << ": ";
-        }
-        std::cout << "expected " << b << " but got " << a << std::endl;
+    if (predicate() == false) {
+        std::cout << message << std::endl;
         exit(1);
     }
+}
+
+template<typename A, typename B>
+void ASSERT_EQUAL(const A& a, const B& b, const std::string& message)
+{
+    ASSERT(
+        [&]() { return a == b; },
+        message + " : expected values to be equal"
+    );
+}
+
+template<typename T>
+void ASSERT_NULL(const T* t, const std::string& message)
+{
+    ASSERT(
+        [&]() { return t == nullptr; },
+        message + " : expected value to be null"
+    );
+}
+
+template<typename T>
+void ASSERT_NOT_NULL(const T* t, const std::string& message)
+{
+    ASSERT(
+        [&]() { return t != nullptr; },
+        message + " : expected value to not be null"
+    );
 }

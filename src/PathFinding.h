6d75aeceef002992e1c49993f2a18186f3277158
Gabriel Lumbi <Lumbi@users.noreply.github.com>
2023-02-24 19:54:20 -0500

    Use replace std::map with std::unordered_map and std::unordered_set

--- src/PathFinding.h
+++ src/PathFinding.h
@@ -1,5 +1,6 @@
 #include <vector>
-#include <map>
+#include <unordered_map>
+#include <unordered_set>
 #include <functional>
 #include <algorithm>
 #include <limits>
@@ -16,9 +17,9 @@ std::vector<Node*> find_shortest_path(
 )
 {
     std::vector<Node*> to_visit;
-    std::map<Key, bool> visited;
-    std::map<Key, float> distance;
-    std::map<Key, Node*> previous;
+    std::unordered_set<Key> visited;
+    std::unordered_map<Key, float> distance;
+    std::unordered_map<Key, Node*> previous;
 
     to_visit.push_back(&start);
     distance[get_key(start)] = 0;
@@ -28,12 +29,12 @@ std::vector<Node*> find_shortest_path(
     // Convenience distance accessor
     auto get_distance = [&](Node& node) -> float {
         Key key = get_key(node);
-        return distance.find(key) != distance.end()
+        return distance.contains(key)
             ? distance[key]
             : std::numeric_limits<float>::infinity();
     };
 
-    while (to_visit.empty() == false) {
+    while (!to_visit.empty()) {
         // Find the nearest node to visit
         auto it_nearest = std::min_element(
             to_visit.cbegin(),
@@ -52,21 +53,23 @@ std::vector<Node*> find_shortest_path(
         }
 
         // Skip if already visited
-        if (visited[get_key(*nearest)]) {
+        Key nearest_key = get_key(*nearest);
+        if (visited.contains(nearest_key)) {
             continue;
         } else {
-            visited[get_key(*nearest)] = true;
+            visited.insert(nearest_key);
         }
 
         // Explore neighbors and calculate distances
         for (auto&& neighbor : get_neighbors(*nearest)) {
-            if (visited[get_key(*neighbor)] == false) {
+            Key neighbor_key = get_key(*neighbor);
+            if (!visited.contains(neighbor_key)) {
                 to_visit.push_back(neighbor);
                 float new_distance = get_distance(*nearest) + 1.f; // default distance to 1 between nodes
                 float known_distance = get_distance(*neighbor);
                 if (new_distance < known_distance) {
-                    distance[get_key(*neighbor)] = new_distance;
-                    previous[get_key(*neighbor)] = nearest;
+                    distance[neighbor_key] = new_distance;
+                    previous[neighbor_key] = nearest;
                 }
             }
         }
@@ -78,7 +81,7 @@ std::vector<Node*> find_shortest_path(
     while (current) {
         path.push_back(current);
         Key key = get_key(*current);
-        if (previous.find(key) != previous.end()) {
+        if (previous.contains(key)) {
             current = previous[key];
         } else {
             current = nullptr;

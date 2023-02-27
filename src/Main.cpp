c1f714f3c6abfe27b670de676ff016154d690662
Gabriel Lumbi <Lumbi@users.noreply.github.com>
2023-02-26 21:35:28 -0500

    Install SFML

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include <SFML/Graphics.hpp>
+
 #include "Condition.h"
 #include "Action.h"
 #include "Goal.h"
@@ -7,5 +9,23 @@
 
 int main()
 {
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }

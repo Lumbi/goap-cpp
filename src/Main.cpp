e22f4c4afb523f2d8a0c1a4e918898fa67f963b1
Gabriel Lumbi <Lumbi@users.noreply.github.com>
2023-03-01 20:07:13 -0500

    Use World in Main

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
+#include <memory>
 #include <SFML/Graphics.hpp>
 
 #include "Critter.h"
+#include "Food.h"
+#include "World.h"
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1024, 768), "GOAP");
     sf::View view = window.getDefaultView();
 
-    Critter critter;
-    critter.position = { 100, 100 };
+    World world;
+
+    for (int i = -5; i < 5; i++) {
+        auto critter = std::make_unique<Critter>(
+            sf::Vector2f({
+                float(500 + (i * 10) * (rand() % 5)),
+                float(300 + (i * 10) * (rand() % 5))
+            })
+        );
+        world.entities.push_back(std::move(critter));
+    }
 
     sf::Event event;
     sf::Clock clock;
+    float target_frame_rate = 60.f;
+    float target_time_per_frame = 1.f / target_frame_rate;
+
+    for (int i = 0; i < 50; i++) {
+        Food::spawn_at_random(world);
+    }
+
     while (window.isOpen())
     {
-        sf::Time deltaTime = clock.restart();
+        sf::Time delta_time = clock.restart();
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
@@ -23,19 +42,24 @@ int main()
 
             if (event.type == sf::Event::Resized)
             {
-                sf::FloatRect visibleArea(0.f, 0.f, event.size.width, event.size.height);
-                window.setView(sf::View(visibleArea));
+                sf::FloatRect visible_area(0.f, 0.f, event.size.width, event.size.height);
+                window.setView(sf::View(visible_area));
             }
         }
 
-        // UPDATE
-        critter.update(deltaTime);
-
-        // DRAW
+        world.update(delta_time);
 
         window.clear();
-        critter.draw(window);
+
+        world.draw(window);
+
         window.display();
+
+        float frame_elapsed_time = clock.getElapsedTime().asSeconds();
+        if (frame_elapsed_time < target_time_per_frame) {
+            float delay = target_time_per_frame - frame_elapsed_time;
+            sf::sleep(sf::seconds(delay));
+        }
     }
 
     return 0;

08e1c7a2d76d798add274da493a048dae5f7703a
Gabriel Lumbi <Lumbi@users.noreply.github.com>
2023-03-01 20:05:51 -0500

    Add World

---
+++ src/World.cpp
@@ -0,0 +1,72 @@
+#include "World.h"
+
+#include <algorithm>
+#include <vector>
+#include <cmath>
+
+#include "Critter.h"
+#include "Food.h"
+
+void World::update(const sf::Time& delta_time)
+{
+    for (auto&& entity : entities) {
+        if (entity) {
+            entity->update(*this, delta_time);
+        }
+    }
+
+    // Spawn food
+    food_spawn_time += delta_time.asSeconds();
+    if (food_spawn_time >= food_spawn_delay) {
+        food_spawn_time = 0;
+        Food::spawn_at_random(*this);
+    }
+
+    // Clean up
+    if (destroyed_count > 0) {
+        std::erase_if(entities, [](auto&& entity){ return entity.get() == nullptr; });
+        destroyed_count = 0;
+    }
+}
+
+void World::draw(sf::RenderTarget& render)
+{
+    for (auto&& entity : entities) {
+        if (entity) {
+            entity->draw(render);
+        }
+    }
+}
+
+void World::destroy(Entity* entity_to_destroy)
+{
+    // TODO: Dangerous and inefficient, would be nice to use a DOD approach instead
+    for (auto&& entity : entities) {
+        if (entity.get() == entity_to_destroy) {
+            delete entity.release();
+            destroyed_count++;
+            break;
+        }
+    }
+}
+
+float World::distance(const sf::Vector2f& a,  const sf::Vector2f& b)
+{
+    float dx = a.x - b.x;
+    float dy = a.y - b.y;
+    return sqrt(dx * dx + dy * dy);
+}
+
+int World::food_count_nearby(const sf::Vector2f& position)
+{
+    const float radius = 128.f;
+    int count = 0;
+    for (auto&& entity : entities) {
+        Food* food = dynamic_cast<Food*>(entity.get());
+        if (!food) { continue; }
+        if (World::distance(position, food->get_position()) <= radius) {
+            count++;
+        }
+    }
+    return count;
+}
---
+++ src/World.h
@@ -0,0 +1,33 @@
+#include <vector>
+#include <memory>
+#include <SFML/Graphics.hpp>
+
+#ifndef WORLD_H
+#define WORLD_H
+
+class World {
+    public:
+        World() = default;
+        World(const World&) = delete;
+        World& operator=(const World&) = delete ;
+
+    public:
+        sf::IntRect bounds = { 0, 0, 1024, 768 };
+        std::vector<std::unique_ptr<class Entity>> entities;
+
+    private:
+        float food_spawn_time = 0.f;
+        float food_spawn_delay = 2.f;
+        int destroyed_count = 0;
+
+    public:
+        void update(const sf::Time&);
+        void draw(sf::RenderTarget&);
+
+        void destroy(Entity*);
+
+        static float distance(const sf::Vector2f&, const sf::Vector2f&);
+        int food_count_nearby(const sf::Vector2f& position);
+};
+
+#endif
